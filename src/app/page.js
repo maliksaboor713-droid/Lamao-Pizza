cf13f69428a91e8d36980ec7c63abc759531086e
Malik <maliksaboor713@gmail.com>
2026-09-09 02:21:58 -0700

    Update homepage

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
 
     <div className='h-[1fr] m-25 gap-10 text-center text-balance flex flex-col justify-around items-center'>
     <div >
-      <h1 className='text-[2rem]  font-(900)'>We sell Tasty pizza</h1>
+      <h1 className='text-[2rem]  font-(900)'>Delicious Pizza at Lamao Pizza</h1>
       <p className='text-[1.2rem] font-(900)'>Our website offers and sell almost all type of amazing flavoured pizza</p>
     </div>
     <div>
